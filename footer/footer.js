13f62ae62bf057f6521ac8c708624a3e872cfff6
imafckingwalkinparadox <hdvillatoro@scl.edu.gt>
2024-05-30 11:44:07 -0600

    Actualizacion footer

--- footer/footer.js
+++ footer/footer.js
@@ -1,3 +1,9 @@
 let footer_Dom = document.querySelector(".footer");
 
-footer_Dom.innerHTML = `footer`;
+footer_Dom.innerHTML = `
+<h1 class="scl">SCL</h1>
+<a href="" class="user">@hdvillatoro</a>
+<a href="" class="user">@sejeronimo</a>
+<a href="" class="user">@sesarazua</a>
+<a href="" class="user">@bemorales</a>
+`;
