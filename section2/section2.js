e44f16beebd6da462a5d81bfd4c9b6c6ef1132e8
SamuelSarazua <sesarazua@scl.edu.gt>
2024-05-30 12:24:06 -0600

    Texto de Sarazua

--- section2/section2.js
+++ section2/section2.js
@@ -1,3 +1,41 @@
 let section2_Dom = document.querySelector(".section2");
 
-section2_Dom.innerHTML = `section2`;
+section2_Dom.innerHTML = `
+<table border="1">
+    <tr>
+        <td>a</td>
+        <td>Representa un hiperenlace.
+        </td>
+    </tr>
+    <tr>
+        <td>em</td>
+        <td>Especifica un texto enfatizado.
+        </td>
+    </tr>
+    <tr>
+        <td>strong</td>
+        <td>Establece un texto importante.
+        </td>
+    </tr>
+    <tr>
+        <td>small</td>
+        <td>Define un comentario aparte, es decir, textos de políticas de responsabilidad o una nota de derechos de autoría</td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Representa contenido que no es exacto, tiene el estilo tachado.
+        </td>
+    </tr>
+    <tr>
+        <td>cite</td>
+        <td>Indica el título de una obra.
+        </td>
+    </tr>
+    <tr>
+        <td>br</td>
+        <td>Inserta un salto de línea.
+        </td>
+    </tr>
+    <br>
+</table>
+`;
